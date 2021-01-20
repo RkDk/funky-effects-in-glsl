6145dacfa0cf31889226cc3fbce0ed0189124e1a
Rick <darkcyborg1@gmail.com>
2021-01-20 01:01:33 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  funky-effects-in-glsl
+//
+//  Created by Rick Chou on 1/20/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
